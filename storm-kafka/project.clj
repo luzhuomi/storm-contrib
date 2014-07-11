ec1b7e7781bc12cc3622465208aa102f2751e15e
Kenny Zhuo Ming Lu <luzhuomi@gmail.com>
2014-07-11 17:30:05 +0800

    fixed the mis-dependency to scala 2.9

--- storm-kafka/project.clj
+++ storm-kafka/project.clj
@@ -3,7 +3,7 @@
   :repositories {"scala-tools" "http://scala-tools.org/repo-releases"
                   "conjars" "http://conjars.org/repo/"}
   :dependencies [[org.scala-lang/scala-library "2.10.2"]
-                  [org.apache.kafka/kafka_2.9.2 "0.8.0"
+                  [org.apache.kafka/kafka_2.10 "0.8.1"
                   :exclusions [org.apache.zookeeper/zookeeper
                                log4j/log4j]]]
   :profiles
